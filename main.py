52c741d864445ce848c651006c98047f92074f5f
ravinegi17 <55530258+ravinegi17@users.noreply.github.com>
2025-07-08 11:51:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from trend_fetcher import get_trending_topic
+from trend_fetcher import get_tech_trending_topic
 from gpt_blog_generator import generate_blog_post
 from image_generator import generate_blog_image
 from wp_poster import post_to_wordpress
 
 def auto_run_blog():
-    topic = get_trending_topic()
+    topic = get_tech_trending_topic()
     print(f"🔥 Selected Topic: {topic}")
     blog = generate_blog_post(topic)
     image_path = generate_blog_image(topic, filename=blog['slug'] + ".jpg")
